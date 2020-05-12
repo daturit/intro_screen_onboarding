e59f4309cd4f1c0f37793a0f8e494da947938bfb
Le Anh Tuan <tuanla1@ssi.com.vn>
2020-05-12 10:14:02 +0700

    add more functions

---
+++ intro_screen_onboarding/example/lib/intro_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:introscreenonboarding/introscreenonboarding.dart';
+import 'package:introscreenonboarding/Introduction.dart';
+
+
+import 'home_page.dart';
+
+class TestScreen extends StatelessWidget {
+
+  final List<Introduction> list = [
+    Introduction(
+      title: 'Buy & Sell',
+      subTitle: 'Browse the menu and order directly from the application',
+      imageUrl: 'assets/images/onboarding3.png',
+
+    ),
+    Introduction(
+      title: 'Delivery',
+      subTitle: 'Your order will be immediately collected and',
+      imageUrl: 'assets/images/onboarding4.png',
+    ),
+    Introduction(
+      title: 'Receive Money',
+      subTitle: 'Pick up delivery at your door and enjoy groceries',
+      imageUrl: 'assets/images/onboarding5.png',
+    ),
+    Introduction(
+      title: 'Finish',
+      subTitle: 'Browse the menu and order directly from the application',
+      imageUrl: 'assets/images/onboarding3.png',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroScreenOnboarding(introductionList: list, onTapSkipButton: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePage(),
+        ), //MaterialPageRoute
+      );
+    },);
+  }
+}
--- intro_screen_onboarding/lib/introscreenonboarding.dart
+++ intro_screen_onboarding/lib/introscreenonboarding.dart
@@ -1,7 +1,203 @@
 library introscreenonboarding;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:introscreenonboarding/Introduction.dart';
+
+import 'circle_progress_bar.dart';
+
+/// A IntroScreen Class.
+
+class IntroScreenOnboarding extends StatefulWidget {
+  final List<Introduction> introductionList;
+  /// Callback on Skip Button Pressed
+  final VoidCallback onTapSkipButton;
+
+  IntroScreenOnboarding({this.introductionList, this.onTapSkipButton});
+
+  @override
+  _IntroScreenOnboardingState createState() => _IntroScreenOnboardingState();
+}
+
+class _IntroScreenOnboardingState extends State<IntroScreenOnboarding> {
+
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentPage = 0;
+  double progressPercent = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      alignment: Alignment.topRight,
+                      child: FlatButton(
+                        onPressed: () {
+                          print('skip');
+                          widget.onTapSkipButton;
+                        },
+                        child: Text(
+                          'Skip',
+                          style: TextStyle(color: Colors.black, fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: Container(
+                    height: 550.0,
+                    child: PageView(
+                      physics: ClampingScrollPhysics(),
+                      controller: _pageController,
+                      onPageChanged: (int page) {
+                        setState(() {
+                          _currentPage = page;
+                        });
+                      },
+                      children: widget.introductionList,
+                    ),
+                  ),
+                ),
+//                Row(
+//                  mainAxisAlignment: MainAxisAlignment.center,
+//                  children: _buildPageIndicator(),
+//                ),
+                _customProgress(),
+                //_buildNextButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNextButton() {
+    return (_currentPage != widget.introductionList.length - 1
+        ? Expanded(
+      child: Align(
+        alignment: FractionalOffset.bottomRight,
+        child: FlatButton(
+          onPressed: () {
+            _pageController.nextPage(
+              duration: Duration(milliseconds: 500),
+              curve: Curves.ease,
+            );
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+//                    Text(
+//                      'Next',
+//                      style: TextStyle(
+//                        color: Colors.black,
+//                        fontSize: 22.0,
+//                      ),
+//                    ),
+            ],
+          ),
+        ),
+      ),
+    )
+        : Expanded(
+      child: Align(
+        alignment: FractionalOffset.bottomRight,
+        child: FlatButton(
+          onPressed: () {
+            print('Start');
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+//                    Text(
+//                      'Start',
+//                      style: TextStyle(
+//                        color: Colors.black,
+//                        fontSize: 22.0,
+//                      ),
+//                    ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 150),
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 8.0,
+      width: isActive ? 24 : 16,
+      decoration: BoxDecoration(
+        color: isActive ? Color(0xFF7B51D3) : Colors.grey,
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+    );
+  }
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> list = [];
+    for (int i = 0; i < widget.introductionList.length; i++) {
+      list.add(i == _currentPage ? _indicator(true) : _indicator(false));
+    }
+    return list;
+  }
+
+  Widget _customProgress() {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          width: 80,
+          height: 80,
+          child: CircleProgressBar(
+            backgroundColor: Colors.white,
+            foregroundColor: Color(0xFF7B51D3),
+            value: ((_currentPage + 1) * 1.0 / widget.introductionList.length),
+          ),
+        ),
+        Container(
+          height: 55,
+          width: 55,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Color(0xFF7B51D3),
+          ),
+          child: IconButton(
+            onPressed: () {
+              _currentPage != widget.introductionList.length - 1 ?
+              _pageController.nextPage(
+                duration: Duration(milliseconds: 500),
+                curve: Curves.ease,
+                // ignore: unnecessary_statements
+              ) : widget.onTapSkipButton;
+            },
+            icon: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.white,
+            ),
+            iconSize: 15,
+          ),
+        )
+      ],
+    );
+  }
 }
