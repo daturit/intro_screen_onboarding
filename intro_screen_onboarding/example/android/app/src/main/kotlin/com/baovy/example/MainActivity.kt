8fb8a4a629c3033a6d15d1964666c9737aef8877
Le Anh Tuan <tuanla1@ssi.com.vn>
2020-05-12 08:40:11 +0700

    init example project

---
+++ intro_screen_onboarding/example/android/app/src/main/kotlin/com/baovy/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.baovy.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ intro_screen_onboarding/lib/introduction.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Introduction extends StatefulWidget {
+  String imageUrl;
+  String title;
+  String subTitle;
+
+  Introduction({this.imageUrl, this.title, this.subTitle});
+
+  @override
+  State<StatefulWidget> createState() {
+    return new IntroductionState();
+  }
+}
+
+class IntroductionState extends State<Introduction> {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: Padding(
+        padding: EdgeInsets.all(40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Image(
+                image: AssetImage(widget.imageUrl),
+                height: 360.0,
+                width: 360.0,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  widget.title,
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.clip,
+                  style:
+                  TextStyle(color: Colors.black, fontSize: 30.0, height: 1.5, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              widget.subTitle,
+              style:
+              TextStyle(color: Colors.black, fontSize: 22.0, height: 1.5),
+              overflow: TextOverflow.clip,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
